a630113a7fea9ce9a925e450dc598a543e7fb8d3
N0790170 <tavian.taylor2018@my.ntu.ac.uk>
2021-04-24 11:31:07 +0100

    mvc setup

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const mongoCredentials = require('./mongoCredentials');
+const linkRoutes = require('./routes/linkRoutes')
 
 //express app
 const app = express();
@@ -10,3 +11,14 @@ dbURI = `mongodb+srv://${mongoCredentials.username}:${mongoCredentials.password}
 mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
 .then((result) => app.listen(3000))
 .catch((err) => console.error(err))
+
+//register view engine
+app.set('view engine', 'ejs');
+
+//middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+//link routes
+app.use('/',linkRoutes);
